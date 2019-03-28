8ca2c0ebff677865419748aa32f9b54f34b751f7
fsappdev <developfsa@gmail.com>
2019-03-28 11:35:29 -0300

    version con ui casi lista

---
+++ src/components/ProductList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Product from './Product';
+import Title from './Title';
+
+import { ProductConsumer } from '../Context';
+
+export default class ProductList extends Component {
+
+   render() {
+      return (
+         <React.Fragment>
+            <div className="py-5">
+               <div className="container">
+                  <Title name="nuestros" title="productos" />
+
+                  <div className="row">
+
+                     <ProductConsumer>
+                        {value => {
+                           return value.products.map(producto => {
+                              return <Product key={producto.id} producto={producto} />;
+                           });
+                        }}
+                     </ProductConsumer>
+
+                  </div>
+               </div>
+            </div>
+         </React.Fragment>
+         //<Product />
+      );
+   }
+}
